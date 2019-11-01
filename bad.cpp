ed822a9634d0fc37eb3eba8da72d6fb432c01f40
gracereyn <50382945+gracereyn@users.noreply.github.com>
2019-11-01 13:39:55 -0400

    added example for issue fix

--- bad.cpp
+++ bad.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+using std::cout;
+using std::endl;
 int main(){
 // Hi, I'm a program!
 int x = 1;
@@ -5,4 +8,6 @@ for(int i = 0; i < 10; i++) {
 cout << i;
 cout << endl;
 }
+if(true)
+cout << "true"; << endl;
 }
